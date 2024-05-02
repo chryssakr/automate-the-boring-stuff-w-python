07e1b5dcc17dd986cea566a57a6e0c0596c9b032
Chryssa Karatza <chryssakaratza@gmail.com>
2024-05-02 17:32:55 +0100

    change code with input() and f strings

--- chapter1/chapter1.py
+++ chapter1/chapter1.py
@@ -1,11 +1,9 @@
 # This program says hello and asks for my name.
 
 print('Hello, world!')
-print('What is your name?')    # ask for their name
-my_name = input()
-print('It is good to meet you, ' + my_name)
-print('The length of your name is:')
-print(len(my_name))
-print('What is your age?')    # ask for their age
-my_age = input()
-print('You will be ' + str(int(my_age) + 1) + ' in a year.')
+my_name = input('What is your name?')
+print(f'It is good to meet you, {my_name}')
+print(f'The length of your name is: {len(my_name)}')
+
+my_age = input('What is your age?')
+print(f'You will be {int(my_age) + 1} in a year.')
--- chapter2/chapter2.py
+++ chapter2/chapter2.py
@@ -1,78 +1,12 @@
-"""while True:
-    print('who are you?')
-    name = input()
+while True:
+    name = input('who are you?')
     if name != 'Joe':
         continue
-    print('Hello, Joe. What is the password? (It is a fish.)')
-    password = input()
+    password = input('Hello, Joe. What is the password? (It is a fish.)')
     if password == 'swordfish':
         break
 print('Access granted.')
-"""
 
-"""
 print('My name is')
 for i in range(5):
-    print('Jimmy (' + str(i) + ')')
-"""
-
-# A rock paper scissors game.
-import random, sys
-print('ROCK, PAPER, SCISSORS')
-# These variables keep track of the number of wins, losses, and ties.
-wins = 0
-losses = 0
-ties = 0
-while True: # The main game loop.
-    print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
-    while True: # The player input loop.
-        print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
-        player_move = input()
-        if player_move == 'q':
-            sys.exit() # Quit the program.
-        if player_move == 'r' or player_move == 'p' or player_move == 's':
-            break # Break out of the player input loop.
-        print('Type one of r, p, s, or q.')
-
-    # Display what the player chose:
-    if player_move == 'r':
-        print('ROCK versus...')
-    elif player_move == 'p':
-        print('PAPER versus...')
-    elif player_move == 's':
-        print('SCISSORS versus...')
-
-    # Display what the computer chose:
-    random_number = random.randint(1, 3)
-    if random_number == 1:
-        computer_move = 'r'
-        print('ROCK')
-    elif random_number == 2:
-        computer_move = 'p'
-        print('PAPER')
-    elif random_number == 3:
-        computer_move = 's'
-        print('SCISSORS')
-
-    # Display and record the win/loss/tie:
-    if player_move == computer_move:
-        print('It is a tie!')
-        ties = ties + 1
-    elif player_move == 'r' and computer_move == 's':
-        print('You win!')
-        wins = wins + 1
-    elif player_move == 'p' and computer_move == 'r':
-        print('You win!')
-        wins = wins + 1
-    elif player_move == 's' and computer_move == 'p':
-        print('You win!')
-        wins = wins + 1
-    elif player_move == 'r' and computer_move == 'p':
-        print('You lose!')
-        losses = losses + 1
-    elif player_move == 'p' and computer_move == 's':
-        print('You lose!')
-        losses = losses + 1
-    elif player_move == 's' and computer_move == 'r':
-        print('You lose!')
-        losses = losses + 1
+    print(f'Jimmy ({i})')
