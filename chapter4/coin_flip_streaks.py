3bc65a393436d6d00932453666dedb18db7e58df
Chryssa Karatza <chryssakaratza@gmail.com>
2024-06-04 12:35:51 +0300

    change coin flip to be 100 times in 10000 experiments

--- chapter4/coin_flip_streaks.py
+++ chapter4/coin_flip_streaks.py
@@ -1,6 +1,15 @@
 import random
 
 def flip(times: int) -> list[str]:
+    """
+    Flips a coin a number of times.
+
+    Args:
+        times (int): The number of times I want to flip the coin.
+
+    Returns:
+        list[str]: A list containing all the coin flips.
+    """
     flips = []
     for _ in range(times):
         if random.randint(0, 1) == 0:
@@ -9,28 +18,44 @@ def flip(times: int) -> list[str]:
             flips.append("T")
     return flips
 
-def find_streaks(flips: list[str]) -> int:
+def find_streaks(flips: list[str]) -> bool:
+    """
+    Finds whether or not there is a streak of 6 in the list.
+
+    Args:
+        flips (list[str]): The list that contains all the coin flips.
+
+    Returns:
+        bool: Returns True/False if there is a streak or not.
+    """
     streaks = 0
     in_row = 0
+    streak_num = 6
+    
     for i in range(1, len(flips)):
         if flips[i] == flips[i - 1]:
             in_row += 1
         else:
-            if in_row == 6:
+            if in_row == streak_num:
                 streaks += 1
             in_row = 0
     # checking if the last ones add to the streak
-    if in_row == 6:
-        streaks += 1
-    return streaks
-# TODO: change number of experiments to 10000 and number or repetitions per exp to 100
+    if in_row == streak_num:
+        return True
+    return False
 
 def main() -> None:
     number_of_experiments = 10000
-    # TODO: make 6 a variable
-    flips = flip(number_of_experiments)
-    streaks = find_streaks(flips)
-    print(f"Chance of streak: {streaks/100}")
+    containing_streak = 0
+    for _ in range(number_of_experiments):
+        flip_times = 100
+        flips = flip(flip_times)    
+        streaks = find_streaks(flips)
+        if streaks:
+            containing_streak += 1
+    chance_of_streak = containing_streak / number_of_experiments * 100
+    chance_of_streak = round(chance_of_streak, 4)
+    print(f"Chance of streak: {chance_of_streak}%")
     
 if __name__ == "__main__":
     main()
