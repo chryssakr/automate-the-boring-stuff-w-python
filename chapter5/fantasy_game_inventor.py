7d920ecc196c303c4a4b543e6c89d6661ace8607
Chryssa Karatza <chryssakaratza@gmail.com>
2024-06-20 21:41:16 +0100

    add to inventory function

--- chapter5/fantasy_game_inventor.py
+++ chapter5/fantasy_game_inventor.py
@@ -8,9 +8,19 @@ def display_inventory(inventory: dict) -> None:
     )
 
 
+def add_to_inventory(inventory: dict, items_to_add: list) -> dict:
+    for item in items_to_add:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
+
 def main():
     inventory = {"rope": 1, "torch": 6, "gold coin": 42, "dagger": 1, "arrow": 12}
     display_inventory(inventory)
+    dragon_loot = ["gold coin", "dagger", "gold coin", "gold coin", "ruby"]
+    updated_inventory = add_to_inventory(inventory, dragon_loot)
+    display_inventory(updated_inventory)
 
 
 if __name__ == "__main__":
