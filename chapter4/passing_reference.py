39a5b63d05616c903210d6fd7e7f5d443eabd34b
Chryssa Karatza <chryssakaratza@gmail.com>
2025-02-03 14:42:42 +0000

    complete migration to jupyter notebook for chapter4

--- chapter4/passing_reference.py
+++ chapter4/passing_reference.py
@@ -1,6 +1,7 @@
 def eggs(some_parameter):
-    some_parameter.append('Hello')
+    some_parameter.append("Hello")  # lists are mutable so this gets modified in place
+
 
 spam = [1, 2, 3]
-eggs(spam)
+eggs(spam)  # spam gets copied to some_parameter and they both refer to the same list
 print(spam)
